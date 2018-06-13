5fbb1283846eaf75836f6e4153d8e4f925a7d60a
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-06-13 13:31:38 +1200

    create a admin namespace route to accomodate admin user sign in
    - add name space 'admin'
    - set admin namespace root
    - combine resources to one line
    - add a logout route for session#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  resources :customer_profiles
-  resources :users
-  resources :properties
+  resources :users, :customer_profiles, :properties
+
+  namespace :admin do
+    resources :users, :customer_profiles, :properties
+    root 'home#index'
+  end
 
   root 'home#index'
 end
