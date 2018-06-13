3328b88381b4440117e720ff4fb553ebbd5ec7d0
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-06-13 13:35:19 +1200

    Login for admin and user is now possible
    - add admin home controller and initial view to index
    - add customer profile show controller and initial view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user
-      render 'new'
+      log_in(user)
+      redirect_after_sign_in(user)
     else
       flash.now[:danger] = 'Login Failed'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def redirect_after_sign_in(user)
+    if user.admin?
+      redirect_to admin_root_path
+    else
+      redirect_to user.customer_profile
+    end
+  end
 end
