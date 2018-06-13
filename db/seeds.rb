b4a91a76aae6cb919ed8c8c665415fb4ee27d7bf
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-06-13 13:16:13 +1200

    add a seed data for a testing user record id:1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(:first_name => 'Toh', :last_name => 'Jan', :email => 'toh@fluxfederation.com', :role => 1, :status => 1)
+
+user1 = User.create(:first_name => 'Bow', :last_name => 'Jan', :email => 'bow@email.com', :role => 0, :status => 1)
+CustomerProfile.create(:user => user1, :phone_number => '0212953395', :card_number => '0440155188980004')
