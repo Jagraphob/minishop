aa4e35bea2857ff95ecada6ff7a6ccaec21fa43c
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-06-13 13:32:15 +1200

    Add a rescue 404 for the whole application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  
+  def render_404
+    render plain: 'Not found', :status => 404
+  end
 end
