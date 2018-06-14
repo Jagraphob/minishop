504392ef305dacd7b3b64756d53ca832fcc2edf7
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-06-14 15:55:57 +1200

    update user model validation and its test
    - set belong_to customer to optional
    - add format validation to email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
-  belongs_to :customer
+  belongs_to :customer, :optional => true
 
   enum role: [ :user, :admin]
   enum status: [ :inactive, :active ]
 
-  validates :email, :presence => true, :uniqueness => true
+  validates :email,
+    :presence => true,
+    :uniqueness => true,
+    :format => { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+    
   validates :role, :presence => true
   validates :status, :presence => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  fixtures :users
+  subject(:user) { User.create(:email => email, :role => role, :status => status)}
+
+  let(:email) { 'toh@email.com' }
+  let(:role) { :user }
+  let(:status) { :active}
 
   describe 'validations' do
-    it 'validates the presence of first_name' do
-      expect(User.new).to validate_presence_of(:first_name)
-    end
-    it 'validates the presence of last_name' do
-      expect(User.new).to validate_presence_of(:last_name)
-    end
     it 'validates the presence of email' do
       expect(User.new).to validate_presence_of(:email)
     end
@@ -21,6 +19,19 @@ RSpec.describe User, type: :model do
     end
     it 'validates the uniquessness of email' do
       expect(User.new).to validate_uniqueness_of(:email)
+    end
+
+    context 'creating new user with valid parameters' do
+      it 'is valid' do
+        expect(user).to be_valid
+      end
+    end
+
+    context 'creating new user with invalid email' do
+      let(:email) { 'abc.com' }
+      it 'is invalid' do
+        expect(user).to_not be_valid
+      end
     end
   end
 end
