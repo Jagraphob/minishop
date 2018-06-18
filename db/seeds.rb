7f5498d3da1fc8df5451562046d472b7ff5c16c0
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-06-18 14:01:07 +1200

    setup seed data for test customer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create(:first_name => 'Toh', :last_name => 'Jan', :email => 'toh@fluxfederation.com', :role => 1, :status => 1)
+User.create(:email => 'toh@fluxfederation.com', :role => 1, :status => 1, :customer_id => nil)
 
-user1 = User.create(:first_name => 'Bow', :last_name => 'Jan', :email => 'bow@email.com', :role => 0, :status => 1)
-CustomerProfile.create(:user => user1, :phone_number => '0212953395', :card_number => '0440155188980004')
+customer1 = Customer.create(:first_name => 'Bow', :last_name => 'Jan', :phone_number => '0212953395', :card_number => '1111222255558888')
+User.create(:email => 'bow@email.com', :role => 0, :status => 1, :customer => customer1)
