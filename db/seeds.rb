df6fd002a576ff419fc491a5c7f1aa9d204ed03b
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-06-19 09:58:33 +1200

    Add seed on the test account for property relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@ User.create(:email => 'toh@fluxfederation.com', :role => 1, :status => 1, :custo
 
 customer1 = Customer.create(:first_name => 'Bow', :last_name => 'Jan', :phone_number => '0212953395', :card_number => '1111222255558888')
 User.create(:email => 'bow@email.com', :role => 0, :status => 1, :customer => customer1)
+property1 = Property.create(:number => '66', :street_name => 'Owen Street', :suburb => 'Belmont', :city => 'Lower Hutt', :region => 'Wellington', :postcode => '5010', :icp_number => 'ICP124578963')
+Ownership.create(:customer_id => customer1.id, :property_id => property1.id, :status => 1)
