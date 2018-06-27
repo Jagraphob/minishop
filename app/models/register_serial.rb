04b65cc46b13e99d6061c9a307746a2d19029816
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-06-28 10:46:28 +1200

    update register serial with a display method that display the format correctly

--- app/models/register_serial.rb
+++ app/models/register_serial.rb
@@ -4,4 +4,8 @@ class RegisterSerial < ApplicationRecord
   validates :meter_number, :presence => true
   validates :register_number, :presence => true
   validates :register_decimals, :presence => true
+
+  def display
+    "#{meter_number}:#{register_number}[#{register_decimals}]"
+  end
 end
