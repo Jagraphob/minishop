6ef5281fe4f98e74939616b9f593cb51202faa6e
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-06-28 10:50:45 +1200

    admin::properties index and show page that include register_serial and customer data

--- app/controllers/admin/properties_controller.rb
+++ app/controllers/admin/properties_controller.rb
@@ -2,5 +2,31 @@ class Admin::PropertiesController < ApplicationController
   layout "admin"
   
   def index
+    if params.has_key? :status
+      @properties = Property.joins(:ownerships).where('ownerships.status == ?', params[:status])
+    else
+      @properties = Property.all
+    end
+    @status = params[:status]
+  end
+
+  def show
+    @property = Property.find(params[:id])
+  end
+
+  def update
+    @property = Property.find(params[:id])
+    if @property.update(property_params)
+      redirect_to admin_property_path(@property)
+    else
+      flash[:error] = @property.errors.full_messages if @property.errors
+      render 'show'
+    end
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:number, :street_name, :suburb, :city, :region, :postcode, :icp_number)
   end
 end
