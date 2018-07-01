674f72eb5ef56bc1e2bd7b09e2bfc5d8e878c9dc
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-07-02 10:18:34 +1200

    Admin is now able to change the ownership status by toggling
    - Statuses : Active, Pending, Archieved

---
+++ app/controllers/admin/ownerships_controller.rb
@@ -0,0 +1,20 @@
+class Admin::OwnershipsController < ApplicationController
+  layout "admin"
+
+  def update
+    session[:return_to] ||= request.referer
+    @ownership = Ownership.find(params[:id])
+    if @ownership.update(ownership_params)
+      redirect_to session.delete(:return_to)
+    else
+      flash[:error] = @ownership.errors.full_messages if @ownership.errors
+      redirect_to session.delete(:return_to)
+    end
+  end
+
+  private
+
+  def ownership_params
+    params.require(:ownership).permit(:status)
+  end
+end
