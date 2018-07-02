8094925cdd322b29cf2bc336c213fa4400d8a108
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-07-03 09:47:39 +1200

    cucumber customer signup testing
    - New Customer Sign up scenario complete
    - Add additional shared steps: click link, click button, see content, see field
    - Add a specific custom step to find a signed up properties within the table

--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -6,6 +6,22 @@ When("I click {string} link") do |string|
   click_link(string)
 end
 
-Then("I should be redirected to {string} page") do |string|
+When("I click {string}") do |string|
+  click_button(string)
+end
+
+When("I fill in {string} with {string}") do |string1, string2|
+  fill_in(string1, :with => string2)
+end
+
+Then("I should be redirected to {string} main page") do |string|
   expect(page).to have_title("Minishop | " + string)
+end
+
+Then("I should see content {string}") do |string|
+  expect(page).to have_content(string)
+end
+
+Then("I should see field {string}") do |string|
+  expect(page).to find_field(string).value
 end
