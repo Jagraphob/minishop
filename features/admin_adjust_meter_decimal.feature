037ca585b131d7286ca3a4f03596490e5697dd53
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-07-12 11:53:34 +1200

    Cucumber features steps details and definition update
    - admin adjust meter reading
    - admin adjust meter decimals
    - update shared steps
    - add custom admin_adjust steps

--- features/admin_adjust_meter_decimal.feature
+++ features/admin_adjust_meter_decimal.feature
@@ -4,4 +4,20 @@ Feature: Admin adjusting meter decimal places
    I want to be able to adjust the decimal places of a meter
    So that I can match the meter display to that of the physical meter
 
-   Scenario: Admin adjusting the decimal places of a meter.
+   Scenario: Admin increase the decimal places of a meter.
+   Given I am logged in as "admin"
+   When I click "Properties" link
+   And I click "View" in the first row of the properties table
+   Then I should see content "Meter Reading"
+   And I should see custom input group field "Decimals" as "2"
+   When I click "+"
+   Then I should see custom input group field "Decimals" as "3"
+
+  Scenario: Admin decrease the decimal places of a meter.
+   Given I am logged in as "admin"
+   When I click "Properties" link
+   And I click "View" in the first row of the properties table
+   Then I should see content "Meter Reading"
+   And I should see custom input group field "Decimals" as "2"
+   When I click "-"
+   Then I should see custom input group field "Decimals" as "1"
--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -21,6 +21,16 @@ When("I fill in {string} with {string}") do |string1, string2|
   fill_in(string1, :with => string2)
 end
 
+When("I click {string} in the first row of the properties table") do |string|
+  page.all(:link, string)[0].click
+end
+
+When("I click {string} at {string} input group") do |string1, string2|
+  input_group = page.find(:css, 'label.input-group-text-label', :text => string2 )
+  input_group_parent = input_group.find(:xpath, '..').find(:xpath, '..')
+  input_group_parent.click_button(string1)
+end
+
 Then("I should be redirected to {string} main page") do |string|
   expect(page).to have_title("Minishop | " + string)
 end
@@ -33,11 +43,11 @@ Then("I should see field {string}") do |string|
   expect(page).to find_field(string).value
 end
 
+Then("I should see {string} field as {string}") do |name, field_value| 
+  expect(find_field(name).value).to eq field_value
+end
+
 Then ("I should see {string} breadcrumb") do |string|
   bc = page.find(:css, 'ol.breadcrumb')
   expect(bc).to have_css('li.breadcrumb-item', text: string)
-end
-
-When("I click {string} in the first row of the properties table") do |string|
-  page.all(:link, string)[0].click
 end
