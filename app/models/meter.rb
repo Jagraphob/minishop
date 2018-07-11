a67095c3c045804ad12fc8274392716b77cb25a7
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-07-11 14:57:33 +1200

    apply custom validation for meter for reading value cannot decrease

--- app/models/meter.rb
+++ app/models/meter.rb
@@ -1,10 +1,16 @@
 class Meter < ApplicationRecord
   belongs_to :property
 
-  validates :reading, :presence => true, :numericality => { true }
-  validates :decimal_position, :presence => true, :numericality { :greater_than_or_equal_to =>0, :less_than_or_equal_to => 4, only_integer: true }
+  validate :reading_stay_or_increase_only
 
-  def display
-    number_with_precision(reading, :precision => decimal_position)
+  validates :reading, :presence => true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :decimal_position, :presence => true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 4, :only_integer => true }
+
+  private
+
+  def reading_stay_or_increase_only
+    if reading && reading_was
+      errors.add(:reading, 'value cannot be decreased') if reading < reading_was
+    end    
   end
 end
