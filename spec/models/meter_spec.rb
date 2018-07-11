f4069b63cb9530723b83d01b230248c137b747b4
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-07-11 14:48:55 +1200

    describe meter model spec

---
+++ spec/models/meter_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Meter, type: :model do
+  fixtures :meters
+
+  describe 'validations' do
+    it 'validates the presence of reading' do
+      expect(Meter.new).to validate_presence_of(:reading)
+    end
+    it 'validates the presence of decimal_position' do
+      expect(Meter.new).to validate_presence_of(:decimal_position)
+    end
+    it 'validates the format of reading' do
+      expect(Meter.new).to_not allow_values("abc","%").for(:reading)
+    end
+    it 'validates reading to be greater than or equal to zero' do
+      expect(Meter.new).to allow_values(0, 1, 200.568, 45359345).for(:reading)
+      expect(Meter.new).to_not allow_values(-4, -9384.365).for(:reading)
+    end
+    it 'validates the format of decimal_position' do
+      expect(Meter.new).to_not allow_values("abc", "$").for(:decimal_position)
+    end
+    it 'validates decimal_position to be between 0 to 4' do
+      expect(Meter.new).to allow_values(0, 1, 2, 3, 4).for(:decimal_position)
+      expect(Meter.new).to_not allow_values(5, 100, -1, -98.325).for(:decimal_position)
+    end
+    it 'validates decimal_position to be integer only' do
+      expect(Meter.new).to_not allow_values(1.4, 2.56, 4.01).for(:decimal_position)
+    end
+    it 'validates reading cannot be decreased' do
+      meter = Meter.first
+      meter.update(:reading => 1233)
+      expect(meter).to_not be_valid
+    end
+  end
+end
+
+
