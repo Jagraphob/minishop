dab32a73edd759dca5c0ebd2c2eafe551b264e43
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-07-23 11:09:05 +1200

    admin::price controller

---
+++ app/controllers/admin/prices_controller.rb
@@ -0,0 +1,23 @@
+class Admin::PricesController < ApplicationController
+  layout "admin"
+
+  def index
+    @prices = Price.all
+  end
+
+  def create
+    @price = Price.new(price_params)
+    if @price.save
+      flash[:success] = 'New price added successfully'
+    else
+      flash[:danger] = @price.errors.full_messages.join(', ')
+    end
+    redirect_to admin_prices_path
+  end
+
+  private
+
+  def price_params
+    params.require(:price).permit(:price, :unit)
+  end
+end
