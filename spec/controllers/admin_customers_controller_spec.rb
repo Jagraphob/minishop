3eceaae4afdc45e19617584d94db79644b2ac2e8
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-07-25 09:28:47 +1200

    add customer controller show spec

--- spec/controllers/admin_customers_controller_spec.rb
+++ spec/controllers/admin_customers_controller_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe Admin::CustomersController, type: :controller do
       expect(response).to render_template("index")
     end
   end
+
+  describe 'GET #show' do
+    context 'the customer exists' do
+      it 'return http success' do
+        get :show, params: { id: Property.first.to_param }
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "the customer doesn't exist" do
+      it "return http response status 404" do
+        get :show, params: { id: (Property.last.id + 1).to_s }
+        expect(response.status).to eq(404)
+      end
+    end
+  end
 end
