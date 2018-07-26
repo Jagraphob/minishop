b4b40026f860efa313c2017cffbb9813d781b530
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-07-26 15:37:10 +1200

    reference to the meter need to be remapped to meter_reading

--- app/helpers/meters_helper.rb
+++ app/helpers/meters_helper.rb
@@ -1,5 +1,5 @@
 module MetersHelper
   def meter_display(meter)
-    number_with_precision(meter.reading, :precision => meter.decimal_position)
+    "#{number_with_precision(meter.meter_readings.last.reading, :precision => meter.decimal_position)} kwh"
   end
 end
